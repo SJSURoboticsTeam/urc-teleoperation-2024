1cede3c6435b04b36510f1e1e7f6a28a27c1e361
Evan Astle <starphys@gmail.com>
2024-01-04 23:08:28 -0800

    Linting

--- src/panes/CameraPane.jsx
+++ src/panes/CameraPane.jsx
@@ -7,12 +7,12 @@ export default function CameraPane () {
   // TODO: replace with context provider and hooks model
   const cameras = [
     { name: 'Chassis Cam', url: 'https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fHww', ratio: 1000 / 563 },
-    { name: 'Mast Cam', url: 'https://www.sjsurobotics.org/assets/img/gallery/current-team.jpeg', ratio: 2798/1573 },
+    { name: 'Mast Cam', url: 'https://www.sjsurobotics.org/assets/img/gallery/current-team.jpeg', ratio: 2798 / 1573 },
     { name: 'Arm Cam', url: 'http://192.168.1.203:8081/', ratio: 16 / 9 }]
   const [currentCamera, setCurrentCamera] = useState(0)
   const containerRef = useRef(null)
 
-  const [anchorPosition, setAnchorPosition] = useState(null);
+  const [anchorPosition, setAnchorPosition] = useState(null)
   const open = Boolean(anchorPosition)
   const handleClick = (e) => {
     e.preventDefault()
@@ -35,35 +35,35 @@ export default function CameraPane () {
 
   const imageStyle = calculateImageDimensions()
   return (
-      <Box
-        ref={containerRef}
-        sx={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
+    <Box
+      ref={containerRef}
+      sx={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <img
+        style={{ ...imageStyle, maxHeight: '100%', maxWidth: '100%' }}
+        alt='Video Feed.'
+        src={cameras[currentCamera].url}
+        onContextMenu={handleClick}
+      />
+      <Menu
+        id='basic-menu'
+        anchorPosition={anchorPosition}
+        anchorReference='anchorPosition'
+        open={open}
+        onClose={handleClose}
+        onChange={(e) => { setCurrentCamera(e.target.value) }}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
         }}
       >
-        <img
-          style={{ ...imageStyle, maxHeight: '100%', maxWidth: '100%' }}
-          alt='Video Feed.'
-          src={cameras[currentCamera].url}
-          onContextMenu={handleClick}
-        />
-        <Menu
-          id="basic-menu"
-          anchorPosition={anchorPosition}
-          anchorReference='anchorPosition'
-          open={open}
-          onClose={handleClose}
-          onChange={(e) => { setCurrentCamera(e.target.value) }}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          {cameras.map((camera, index) => (<MenuItem key={`${camera.name}-${index}`} onClick={() => {setCurrentCamera(index); setAnchorPosition(null)}}>{camera.name}</MenuItem>))}
-        </Menu>
-      </Box>
+        {cameras.map((camera, index) => (<MenuItem key={`${camera.name}-${index}`} onClick={() => { setCurrentCamera(index); setAnchorPosition(null) }}>{camera.name}</MenuItem>))}
+      </Menu>
+    </Box>
   )
 }
