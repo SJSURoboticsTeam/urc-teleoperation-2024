3e5b461e572b955690774369b9fdc575fe5af9f9
Evan Astle <starphys@gmail.com>
2024-01-06 23:32:17 -0800

    Added manual input pane with subcomponents for arm and drive

--- src/panes/index.jsx
+++ src/panes/index.jsx
@@ -1,8 +1,10 @@
+import ManualInputPane from './ManualInput'
 import TestComponent from './TestComponent'
 import SplitPane from './SplitPane'
 
 const panes = {
   test: { title: 'Test', Component: TestComponent },
+  manualInput: { title: 'Manual Input', Component: ManualInputPane },
   vSplit: { title: 'Split Vertically', Component: (props) => (<SplitPane {...props} direction='row' />) },
   hSplit: { title: 'Split Horizontally', Component: (props) => (<SplitPane {...props} />) }
 }
