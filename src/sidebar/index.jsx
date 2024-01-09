2ffc5fb94a93c1c28da1a176f0207cddbcf7981f
Evan Astle <starphys@gmail.com>
2024-01-08 23:53:55 -0800

    Basic structure/UI for Controller components

--- src/sidebar/index.jsx
+++ src/sidebar/index.jsx
@@ -1,11 +1,11 @@
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard'
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
 
-import Controllers from './Controllers'
+import ControllersSidebar from './ControllersSidebar'
 import Layouts from './Layouts'
 
 const sidebars = {
-  controllers: { title: 'Controllers', Icon: SportsEsportsIcon, Component: Controllers },
+  controllers: { title: 'Controllers', Icon: SportsEsportsIcon, Component: ControllersSidebar },
   layouts: { title: 'Layouts', Icon: SpaceDashboardIcon, Component: Layouts }
 }
 
