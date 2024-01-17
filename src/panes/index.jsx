1956a29a7ac61fa93cd8ade49f0e40ad0ffdb079
bork <bork.andrew.ryan@gmail.com>
2024-01-16 20:28:48 -0800

    Registered ArmViewport as selectable component

--- src/panes/index.jsx
+++ src/panes/index.jsx
@@ -2,13 +2,15 @@ import CameraPane from './CameraPane'
 import ManualInputPane from './ManualInput'
 import TestComponent from './TestComponent'
 import SplitPane from './SplitPane'
+import { ArmViewport } from './3DArmViewport'
 
 const panes = {
   test: { title: 'Test', Component: TestComponent },
   camera: { title: 'Camera', Component: CameraPane },
   manualInput: { title: 'Manual Input', Component: ManualInputPane },
   vSplit: { title: 'Split Vertically', Component: (props) => (<SplitPane {...props} direction='row' />) },
-  hSplit: { title: 'Split Horizontally', Component: (props) => (<SplitPane {...props} />) }
+  hSplit: { title: 'Split Horizontally', Component: (props) => (<SplitPane {...props} />) },
+  armViewport3D: { title: '3D Arm Viewport', Component: ArmViewport}
 }
 
 export default panes
