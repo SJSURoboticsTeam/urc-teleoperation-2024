3c8bc2da158ee26b41f52eb5a0d74e45e5623895
ninaw04 <ninawangcn@gmail.com>
2025-01-04 13:31:56 -0800

    :sparkles: can message formatting done

--- src/utils/Translation.jsx
+++ src/utils/Translation.jsx
@@ -1,15 +1,7 @@
-
-/* arm formatting
-    base
-    shoulder
-    elbow
-    roll
-    pitch
-    yaw
-*/
-
 // arm default context set in App.jsx 
 // arm:{"speed":0,"rotunda":0,"elbow":0,"shoulder":0,"wristPitch":0,"wristRoll":0,"endEffector":0}
+
+// t, id, length
 const armMap = {
     "base": "t1124",
     "shoulder": "t1134",
@@ -19,74 +11,71 @@ const armMap = {
     "yaw": "t1174"
 }
 
+// Converts user input stored in arm commands context into CAN frames
 function armToCan(arm) {
     const armMessages = [];
     for (let key in arm) {
         let payload = armMap[key];
         payload += arm[key].toString(16).padStart(4, "0");
+        payload += "\r";
         armMessages.push(payload);
     }
     return armMessages;
 }
 
 function homeArm() {
-
+    return "t1110\r";
 }
 
 function keyBoardType() {
-
+    return "t1200\r";
 }
 
+// t, id, length
 const driveMap = {
-    // Identifiers already determined in hex
-    "spin": "t101",
-    "drive": "t102",
-    "translate": "t103",
+    "spin": "t1010",
+    "drive": "t1020",
+    "translate": "t1030",
     "speedAngle": "t1048", // id: 104 len: 8
-    "spinOffset": "t1058", 
-    // do I include drive home in here?
 };
 
-/* drive formatting
-    string mode
-    float? speed
-    float? angle
-*/
 // Converts user input stored in drive commands context into CAN frames
 function driveToCan(drive) {
     // if steer mode transmit 
     const driveMessages = [];
 
     // MODE MESSAGE
-    // let mode = "t" // t stands for standard 11 bit CAN frame
-    let mode = ""
-    if (driveMap[drive.mode]) {
+    let mode = "";
+    if (driveMap[drive.mode]) { // should be spin, drive, translate
         mode += driveMap[drive.mode];
+        mode += "\r";
         console.log(mode);
     } else {
         console.log("uh oh mode wrong");
     }
-    driveMessages.push(mode.padStart(3, "0"))
+    driveMessages.push(mode);
 
     // SPEED AND ANGLE MESSAGE
     // id:104 len: 8 0-3: signed 32 bit speed 4-7: signed 32 bit angle -360 to 360
     let speedAngle = driveMap["speedAngle"];
     speedAngle += drive.speed.toString(16).padStart(4, "0");
     speedAngle += drive.angle.toString(16).padStart(4, "0");
-    driveMessages.push(speedAngle)
+    speedAngle += "\r";
+    driveMessages.push(speedAngle);
 
-    // SPIN MODE OFFSET
-    let spinOffset = driveMap["spinOffset"];
-
-    return driveMessages
+    return driveMessages;
 }
 
 function homeDrive() {
-
+    return "t1060\r"
 }
 
-function spinModeOffset() {
-
+function spinModeOffset(x, y) {
+    let payload = "t1058";
+    payload += x.toString(16).padStart(4, "0");
+    payload += y.toString(16).padStart(4, "0");
+    payload += "\r";
+    return payload;
 }
 
 // science
