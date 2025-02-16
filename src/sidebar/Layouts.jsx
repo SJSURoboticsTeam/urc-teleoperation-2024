46dfaa71d4502a5bf1b52051f3bc11da130278a8
ninaw04 <ninawangcn@gmail.com>
2025-02-15 17:13:35 -0800

    :sparkles: working skeleton for future layout configurations

--- src/sidebar/Layouts.jsx
+++ src/sidebar/Layouts.jsx
@@ -1,28 +1,56 @@
-import { Box, Button } from '@mui/material'
-import Typography from '@mui/material/Typography'
+import { Box, Button, Grid } from '@mui/material'
 
 
 import { useLayouts } from '../contexts/LayoutContext'
 
 const layouts = {
   front : { type: 'vSplit', children: [{ type: 'status' }, { type: 'camera'}] },
-  science : { type: 'vSplit', children: [{ type: 'manual' }, { type: 'camera' }]}
+  science : { type: 'vSplit', children: [{ type: 'manualInput' }, { type: 'hSplit', children: [{type: 'camera' }, {type: 'manualInput'}]}]}
 }
 
 export default function Layouts () {
   const [,setLayout] = useLayouts()
   
   return (
-    <Box sx={{ width: 1, height: 1, position: 'relative' }}>
-      <Button variant="contained" onClick={() => setLayout(layouts.front)}>
+    <Box sx={{ width: 1, height: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.front)}>
+            Front View
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.science)}>
+            Science View
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.science)}>
+            Layout 3
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.science)}>
+            Layout 4
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.science)}>
+            Layout 5
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="outlined" fullWidth onClick={() => setLayout(layouts.science)}>
+            Layout 6
+          </Button>
+        </Grid>
+      </Grid>
+      {/* <Button variant="outlined" onClick={() => setLayout(layouts.front)}>
           Front View
-      </Button>
-      <Button variant="contained" onClick={() => setLayout(layouts.science)}>
+      </Button> */}
+      {/* <Button variant="outlined" onClick={() => setLayout(layouts.science)}>
           Science
-      </Button>
-      <Typography>
-        Layout saving and loading options go here
-      </Typography>
+      </Button> */}
     </Box>
   )
 }
